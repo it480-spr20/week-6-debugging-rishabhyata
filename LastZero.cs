d05d59dfa13a4d495b528e8e0dee035f737463a3
Rishabh <59940133+rishabhyata@users.noreply.github.com>
2020-02-26 13:42:17 -0600

    Add files via upload
    Part 3

---
+++ LastZero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 2, 1, 0 };
+            int result = lastZero(inputArray);
+            Console.WriteLine("Expected result is 2, actual result is " + result);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        // If x==null throw NullPointerException
+        // else return the index of the last 0 in x.
+        // If 0 does not occur in x, return -1.
+        static int lastZero(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
